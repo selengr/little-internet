888a09e781906f52c6e30c39f88ec40707de6465
selengr <reza1997karbakhsh@gmail.com>
2026-08-21 00:37:24 +0330

    Added a custom scroll progress bar to the homepage

--- components/custom-scrollbar.tsx
+++ components/custom-scrollbar.tsx
@@ -22,7 +22,7 @@ export function CustomScrollbar() {
   return (
     <div
       aria-hidden
-      className="pointer-events-none fixed bottom-[50px] lg:bottom-[100px] left-8 z-40 h-[174px] w-[3px] rounded-full bg-border md:left-10 lg:left-12 2xl:left-20"
+      className="pointer-events-none fixed bottom-[50px] lg:bottom-[100px] left-8 z-40 h-[124px] lg:h-[174px] w-[3px] rounded-full bg-border md:left-10 lg:left-12 2xl:left-20"
     >
       <div
         className="absolute bottom-0 w-full rounded-full bg-foreground/75 transition-all duration-150 ease-out dark:bg-foreground/85"
