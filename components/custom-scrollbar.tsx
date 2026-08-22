c7f36d528c42fb6ba7fa29114e7b9e5a631c5f9a
selengr <reza1997karbakhsh@gmail.com>
2026-08-22 23:55:55 +0330

    Added a custom scroll progress bar to the homepage - fix

--- components/custom-scrollbar.tsx
+++ components/custom-scrollbar.tsx
@@ -9,7 +9,9 @@ type CustomScrollbarProps = {
 
 export function CustomScrollbar({ mobileOnly = false }: CustomScrollbarProps) {
   const [scrollPercentage, setScrollPercentage] = useState(0)
-
+// testuntuk
+// testuntuk
+// testuntuk
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY
